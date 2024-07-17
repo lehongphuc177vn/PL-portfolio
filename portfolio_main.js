15c90320d0e631378ddb054825e9a28daadee1a8
lehongphuc177vn <131288471+lehongphuc177vn@users.noreply.github.com>
2024-07-17 23:52:52 +0700

    Add files via upload

---
+++ portfolio_main.js
@@ -0,0 +1,108 @@
+// alert("hi");
+
+// NAVBAR
+
+$(".cv-title a").mouseover(function(){
+    $(".cv-title a").css("text-decoration-line", "line-through");
+});
+$(".cv-title a").mouseout(function(){
+    $(".cv-title a").css("text-decoration-line", "none");
+});
+$(".portfolio-title a").mouseover(function(){
+    $(".portfolio-title a").css("text-decoration-line", "line-through");
+});
+$(".portfolio-title a").mouseout(function(){
+    $(".portfolio-title a").css("text-decoration-line", "none");
+});
+// $(".contact-title a").mouseover(function(){
+//     $(".contact-title a").css("text-decoration", "line-through");
+// })
+// $(".contact-title").mouseout(function(){
+//     $(".contact-title").css("text-decoration", "none");
+// })
+
+// DROPDOWN
+
+$(".dropBtn").mouseover(function(){
+    $(".dropBtn").css("font-weight", "500");
+    $(".dropbtn-lang").css("text-decoration", "line-through");
+});
+$(".dropDown").mouseout(function(){
+    $(".dropBtn").css("font-weight", "300");
+    $(".dropbtn-lang").css("text-decoration", "none");
+    $(".dropbtn-arrow").text("▾")
+});
+
+$(".dropContent").mouseover(function(){
+    $(".dropContent-textVi").css("font-weight", "500");
+    $(".dropContent-textVi").css("text-decoration", "line-through");
+    $(".dropbtn-arrow").text("▴");
+});
+$(".dropContent").mouseout(function(){
+    $(".dropContent-textVi").css("font-weight", "300");
+    $(".dropContent-textVi").css("text-decoration", "none");
+});
+
+// SIDEBAR
+$(".active-sideBar ").mouseover(function(){
+    $(".active-sideBar").css("font-weight", "500");
+    $(".active-sideBar").css("text-decoration", "line-through");
+});
+$(".active-sideBar").mouseout(function(){
+    $(".active-sideBar").css("font-weight", "500");
+    $(".active-sideBar").css("text-decoration", "none");
+});
+
+$(".slot").mouseover(function(){
+    $(".slot").css("font-weight", "500");
+    $(".slot").css("text-decoration", "line-through");
+});
+$(".slot").mouseout(function(){
+    $(".slot").css("font-weight", "300");
+    $(".slot").css("text-decoration", "none");
+});
+    
+
+$(".mobile").mouseover(function(){
+    $(".mobile").css("font-weight", "500");
+    $(".mobile").css("text-decoration", "line-through");
+});
+$(".mobile").mouseout(function(){
+    $(".mobile").css("font-weight", "300");
+    $(".mobile").css("text-decoration", "none");
+});
+
+$(".personal").mouseover(function(){
+    $(".personal").css("font-weight", "500");
+    $(".personal").css("text-decoration", "line-through");
+});
+$(".personal").mouseout(function(){
+    $(".personal").css("font-weight", "300");
+    $(".personal").css("text-decoration", "none");
+    
+});
+
+$(".else").mouseover(function(){
+    $(".else").css("font-weight", "500");
+    $(".else").css("text-decoration", "line-through");
+});
+$(".else").mouseout(function(){
+    $(".else").css("font-weight", "300");
+    $(".else").css("text-decoration", "none");
+});
+
+// BACKTOTOP
+
+$(".backToTop").mouseover(function(){
+    $(".backToTop").css("font-weight", "500");
+    $(".backToTop").css("text-decoration", "line-through");
+});
+$(".backToTop").mouseout(function(){
+    $(".backToTop").css("font-weight", "300");
+    $(".backToTop").css("text-decoration", "none");
+});
+
+
+
+
+
