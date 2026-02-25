44c2c8ee0d49ba95ef051df574fd156845c6137e
lehongphuc177vn <131288471+lehongphuc177vn@users.noreply.github.com>
2026-02-26 00:37:57 +0700

    Add files via upload

--- public/Mobile/projects_mobile.js
+++ public/Mobile/projects_mobile.js
@@ -1,3 +1,23 @@
+//PASSWORD BROWSER
+
+// (function () {
+//   const PASSWORD = "phelac177";
+//   const KEY = "unlock_ok";
+
+  
+//   if (sessionStorage.getItem(KEY) === "1") return;
+
+//   const pass = prompt("Enter password");
+//   if (pass === PASSWORD) {
+//     sessionStorage.setItem(KEY, "1");
+//   } else {
+//     document.documentElement.innerHTML = "";
+//     alert("Wrong password.");
+//     location.replace("/");
+//   }
+// })();
+
+
 // PASSWORD
 
 document.addEventListener("DOMContentLoaded", function () {
