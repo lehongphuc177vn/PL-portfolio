2a7ca600847729af86fe6af129dbe7da9bcc59b9
lehongphuc177vn <131288471+lehongphuc177vn@users.noreply.github.com>
2026-02-26 00:37:21 +0700

    Add files via upload

--- public/Slot/projects_slot.js
+++ public/Slot/projects_slot.js
@@ -17,26 +17,26 @@
   }
 })();
 
-// PASSWORD
+// // PASSWORD
 
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-  const PASSWORD = "phelac177";
+//   const PASSWORD = "phelac177";
 
-  document.querySelectorAll(".locked").forEach(link => {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
+//   document.querySelectorAll(".locked").forEach(link => {
+//     link.addEventListener("click", function(e) {
+//       e.preventDefault();
 
-      const pass = prompt("Enter password");
-      if (pass === PASSWORD) {
-        window.location.href = this.href;
-      } else {
-        alert("Wrong password. Please try again.");
-      }
-    });
-  });
+//       const pass = prompt("Enter password");
+//       if (pass === PASSWORD) {
+//         window.location.href = this.href;
+//       } else {
+//         alert("Wrong password. Please try again.");
+//       }
+//     });
+//   });
 
-});
+// });
 
 // NAVBAR
 
